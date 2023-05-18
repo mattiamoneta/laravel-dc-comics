dcae484b2e8070893b32510f041db1e42c57346f
mattiamoneta <in.mattiamoneta@gmail.com>
2023-05-18 15:38:58 +0200

    BONUS (Custom validation messages)

--- app/Http/Controllers/Generic/ComicController.php
+++ app/Http/Controllers/Generic/ComicController.php
@@ -46,6 +46,22 @@ class ComicController extends Controller
             'series'=>'required|max:80',
             'sale_date'=>'required|max:12',
             'type'=>'required|max:255',
+        ],[
+            'title.required'=>'Il campo Title non può essere vuoto',
+            'description.required'=>'Il campo Description non può essere vuoto',
+            'thumb.required'=>'Il campo Thumbnail non può essere vuoto',
+            'thumb.url'=>'Il campo non contiene un URL valido',
+            'price.required'=>'Il campo Price non può essere vuoto',
+            'series.required'=>'Il campo Serie non può essere vuoto',
+            'sale_date.required'=>'Il campo Sale Date non può essere vuoto',
+            'type.required'=>'Il campo Type non può essere vuoto',
+
+            'title.max'=>'Il campo Title non può essere superiore a 80 caratteri',
+            'description.max'=>'Il campo Description non può essere  superiore a 65535 caratteri',
+            'price.max'=>'Il campo Price non può essere superiore a 4 caratteri',
+            'series.max'=>'Il campo Serie non può essere superiore a 80 caratteri',
+            'sale_date.max'=>'Il campo Sale Date non può essere superiore a 12 caratteri',
+            'type.max'=>'Il campo Type non può essere superiore a 80 caratteri'
         ]);
 
         $data = $request->all();
@@ -108,7 +124,23 @@ class ComicController extends Controller
             'price'=>'required|max:4',
             'series'=>'required|max:80',
             'sale_date'=>'required|max:12',
-            'type'=>'required|max:255'
+            'type'=>'required|max:80'
+        ],[
+            'title.required'=>'Il campo Title non può essere vuoto',
+            'description.required'=>'Il campo Description non può essere vuoto',
+            'thumb.required'=>'Il campo Thumbnail non può essere vuoto',
+            'thumb.url'=>'Il campo non contiene un URL valido',
+            'price.required'=>'Il campo Price non può essere vuoto',
+            'series.required'=>'Il campo Serie non può essere vuoto',
+            'sale_date.required'=>'Il campo Sale Date non può essere vuoto',
+            'type.required'=>'Il campo Type non può essere vuoto',
+
+            'title.max'=>'Il campo Title non può essere superiore a 80 caratteri',
+            'description.max'=>'Il campo Description non può essere  superiore a 65535 caratteri',
+            'price.max'=>'Il campo Price non può essere superiore a 4 caratteri',
+            'series.max'=>'Il campo Serie non può essere superiore a 80 caratteri',
+            'sale_date.max'=>'Il campo Sale Date non può essere superiore a 12 caratteri',
+            'type.max'=>'Il campo Type non può essere superiore a 80 caratteri'
         ]);
 
         $form_data = $request->all();
